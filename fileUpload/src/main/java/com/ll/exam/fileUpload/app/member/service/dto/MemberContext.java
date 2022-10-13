8757732f9cb319db46663acea7f7a101270f2212
leewooyup <zjadlspun1114@naver.com>
2022-10-13 21:41:10 +0900

    회원정보수정날짜를 cacheKey로 사용

--- fileUpload/src/main/java/com/ll/exam/fileUpload/app/member/service/dto/MemberContext.java
+++ fileUpload/src/main/java/com/ll/exam/fileUpload/app/member/service/dto/MemberContext.java
@@ -2,10 +2,13 @@ package com.ll.exam.fileUpload.app.member.service.dto;
 
 import com.ll.exam.fileUpload.app.member.entity.Member;
 import lombok.Getter;
+import lombok.Setter;
+import net.bytebuddy.asm.Advice;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,6 +19,8 @@ import java.util.stream.Collectors;
 public class MemberContext extends User implements OAuth2User {
     private final Long id;
     private final String email;
+    @Setter
+    private LocalDateTime modifyDate;
     private final String profileImgUrl;
     private Map<String, Object> attributes;
     private String userNameAttributeName;
@@ -24,6 +29,7 @@ public class MemberContext extends User implements OAuth2User {
         super(member.getUsername(), member.getPassword(), authorities);
         this.id = member.getId();
         this.email = member.getEmail();
+        this.modifyDate = member.getModifyDate();
         this.profileImgUrl = member.getProfileImgUrl();
     }
 
@@ -48,6 +54,6 @@ public class MemberContext extends User implements OAuth2User {
     }
 
     public String getProfileImgRedirectUrl() {
-        return "/member/profile/img/" + getId() +"?random=" + UUID.randomUUID();
+        return "/member/profile/img/" + getId() +"?cacheKey=" + getModifyDate().toString();
     }
 }
