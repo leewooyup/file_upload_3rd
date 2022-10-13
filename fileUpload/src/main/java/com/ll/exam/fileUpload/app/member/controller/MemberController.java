94643562abca36ffe212698bff6088adf6920a4f
leewooyup <zjadlspun1114@naver.com>
2022-10-13 11:08:17 +0900

    브라우저캐시 설정

--- fileUpload/src/main/java/com/ll/exam/fileUpload/app/member/controller/MemberController.java
+++ fileUpload/src/main/java/com/ll/exam/fileUpload/app/member/controller/MemberController.java
@@ -4,6 +4,10 @@ import com.ll.exam.fileUpload.app.member.entity.Member;
 import com.ll.exam.fileUpload.app.member.service.MemberService;
 import com.ll.exam.fileUpload.app.member.service.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,7 +19,10 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.security.Principal;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/member")
@@ -63,7 +70,11 @@ public class MemberController {
     }
 
     @GetMapping("/profile/img/{id}")
-    public String showProfileImg(@PathVariable Long id) {
-        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
+    public ResponseEntity<Object> showProfileImg(@PathVariable Long id) throws URISyntaxException {
+        URI redirecUri = new URI(memberService.getMemberById(id).getProfileImgUrl());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirecUri);
+        httpHeaders.setCacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS));
+        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
     }
 }
